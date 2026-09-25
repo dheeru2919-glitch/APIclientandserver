da010941f885008b208001df608db516c076309f
dheeru2919-glitch <dheeru2919@gmail.com>
2026-09-25 15:04:57 +0530

    WPF_Application_client

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Call_your_existing_Patient_API.ViewModel;
+using System.Net.Http;
+using System.Windows;
+
+namespace Call_your_existing_Patient_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new PatientViewModel();
+        }
+    }
+}
+
